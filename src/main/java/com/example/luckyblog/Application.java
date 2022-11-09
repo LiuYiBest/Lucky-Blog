e88125cf02aae7180b0c52bee5378b93b412a04d
liuyidiao <1070692044@qq.com>
2022-11-09 21:37:41 +0800

    修改index

--- src/main/java/com/example/luckyblog/Application.java
+++ src/main/java/com/example/luckyblog/Application.java
@@ -10,11 +10,9 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
