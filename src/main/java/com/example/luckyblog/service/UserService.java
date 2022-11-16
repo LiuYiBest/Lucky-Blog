ddb56081ac3de804fd7d4e8da42512646f782d95
liuyidiao <1070692044@qq.com>
2022-11-16 20:35:40 +0800

    获取用户密码

--- src/main/java/com/example/luckyblog/service/UserService.java
+++ src/main/java/com/example/luckyblog/service/UserService.java
@@ -4,11 +4,24 @@ import com.example.luckyblog.entity.User;
 import com.example.luckyblog.mapper.UserMapper;
 
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author liuyidiao
  */
 public class UserService {
+
+    private Map<String,String> userPasswords =new ConcurrentHashMap<>() ;
+    public void save(String username,String password){
+        userPasswords.put(username,password);
+    }
+    public String getPassword(String username){
+        return userPasswords.get(username);
+    }
+
     private UserMapper userMapper;
 
     @Inject
